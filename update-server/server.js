0f620f586e1a53a3416c345a06c1659dc030fe94
Alex Stupak <alex.stupak@meetkasper.com>
2024-11-01 12:35:59 +0100

    update mac build

--- update-server/server.js
+++ update-server/server.js
@@ -4,6 +4,23 @@ const path = require('path');
 const app = express();
 const PORT = 8080;
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/updates/:env/latest-mac.yml', (req, res) => {
+  // console.log("call", req)
+  const { env } = req.params; // production або staging
+
+  if (env !== 'production' && env !== 'staging') {
+    return res.status(400).send('Invalid environment');
+  }
+
+  const filePath = path.join(__dirname, 'updates', env, 'latest-mac.yml');
+  res.sendFile(filePath);
+});
+
 app.get('/updates/:env/latest.yml', (req, res) => {
   const { env } = req.params; // production або staging
 
