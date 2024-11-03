676f2a7afa8bc023f6c7965018477dfcbd624818
Alex Stupak <alex.stupak@meetkasper.com>
2024-11-03 12:18:49 +0100

    clean up

--- src/main/main.ts
+++ src/main/main.ts
@@ -98,45 +98,6 @@ if (isDebug) {
 }
 
 const createWindow = async () => {
-  // const customSession = session.fromPartition('persist:tab-1', { cache: true });
-
-  // const cookies = [
-  //   {
-  //     url: 'https://onlyfans.com',
-  //     name: 'sess',
-  //     value: '2fbcilfq7a7e0ms68h3jd95n7b',
-  //     httpOnly: true,
-  //     secure: true,
-  //   },
-  //   {
-  //     url: 'https://onlyfans.com',
-  //     name: '_cfuvid',
-  //     value:
-  //       'R6Cy8f.ASQAuacdwq9sVyV08W4ix_Tw0dco0OgSUBqs-1730013197638-0.0.1.1-604800000',
-  //     httpOnly: true,
-  //     secure: true,
-  //   },
-  //   {
-  //     url: 'https://onlyfans.com',
-  //     name: 'lang',
-  //     value: 'en',
-  //     httpOnly: true,
-  //     secure: true,
-  //   },
-  //   {
-  //     url: 'https://onlyfans.com',
-  //     name: 'auth_id',
-  //     value: '394757173',
-  //     httpOnly: true,
-  //     secure: true,
-  //   },
-  // ];
-
-  // cookies.forEach((cookie) => {
-  //   customSession.cookies.set(cookie).catch((error) => {
-  //     console.error('Error setting cookie:', error);
-  //   });
-  // });
 
   mainWindow = new BrowserWindow({
     show: false,
@@ -154,23 +115,6 @@ const createWindow = async () => {
   });
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
-
-  // mainWindow.webContents.on('did-finish-load', async () => {
-  //   const cookies = [
-  //     { url: 'https://onlyfans.com', name: 'sess', value: '2fbcilfq7a7e0ms68h3jd95n7b', httpOnly: true, secure: true },
-  //     { url: 'https://onlyfans.com', name: '_cfuvid', value: 'R6Cy8f.ASQAuacdwq9sVyV08W4ix_Tw0dco0OgSUBqs-1730013197638-0.0.1.1-604800000', httpOnly: true, secure: true },
-  //     { url: 'https://onlyfans.com', name: 'lang', value: 'en', httpOnly: true, secure: true },
-  //     { url: 'https://onlyfans.com', name: 'auth_id', value: '394757173', httpOnly: true, secure: true },
-  //   ];
-
-  //   // Send cookies to IPC handler
-  //   mainWindow.webContents.send('set-cookies', cookies);
-
-  //   // Set localStorage after loading
-  //   mainWindow.webContents.executeJavaScript(`
-  //     localStorage.setItem('bcTokenSha', '95ad16528ee36510d580788ccf4123ca914e3498');
-  //   `);
-  // });
 
   mainWindow.on('ready-to-show', () => {
     if (mainWindow) mainWindow.show();
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -6,16 +6,12 @@ const root = createRoot(container);
 root.render(
   <div>
     <App />
-    {/* <button onClick={() => window.electron.ipcRenderer.sendMessage('ipc-example', ['ping'])}>
-      Send Message
-    </button> */}
   </div>
 );
-// root.render(<div>hi</div>)
 
-// calling IPC exposed from preload script
-window.electron.ipcRenderer.once('ipc-example', (arg) => {
-  console.log("log pong")
-  // eslint-disable-next-line no-console
-  console.log(arg);
-});
+// // calling IPC exposed from preload script
+// window.electron.ipcRenderer.once('ipc-example', (arg) => {
+//   console.log("log pong")
+//   // eslint-disable-next-line no-console
+//   console.log(arg);
+// });
